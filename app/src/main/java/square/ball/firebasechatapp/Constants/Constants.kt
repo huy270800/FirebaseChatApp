49cfef65b4b0ddcf54b82a96e3a52da1618894ca
Quoc Huy <55485803+huy270800@users.noreply.github.com>
2021-04-06 16:28:40 +0300

    Update Constants.kt

--- app/src/main/java/square/ball/firebasechatapp/Constants/Constants.kt
+++ app/src/main/java/square/ball/firebasechatapp/Constants/Constants.kt
@@ -3,7 +3,7 @@ package com.codingwithme.firebasechat.Constants
 class Constants {
     companion object{
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAh4BdVdA:APA91bFj3bL9u74QZrTLajVNfGc5-fJHVf-TUYOZgJai4nxI20nqwSbdLAV4cT644fRbnEjNqKwoGKIq5WTp99s4kGvHff6cvbaHZAiBFgyt1Y4qRgztoLuiBBcEi3WnhmebA7xgJgF_" // get firebase server key from firebase project setting
+        const val SERVER_KEY = "" // get firebase server key from firebase project setting
         const val CONTENT_TYPE = "application/json"
     }
 }
